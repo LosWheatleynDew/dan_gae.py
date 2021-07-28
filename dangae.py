e0c5c351e23dca8e79378f9c5e2c2cfec2b270dd
LosWheatleynDew <75549894+LosWheatleynDew@users.noreply.github.com>
2021-07-28 13:28:40 -0700

    Dan is gae
    Dan is gae

---
+++ dangae.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+from discord.utils import get
+import re
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_message(message):
+    print(message.content)
+    if message.content.lower() == "i am gae":
+        print(message)
+    else:
+        await message.delete()
+
+bot.run('token') 
