c14577055b6e9b853026c9a8fe0494656d8b46fd
Marie <marie@mxy.gg>
2021-07-28 23:19:17 +0200

    Improve gae

--- dangae.py
+++ dangae.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 import re
+import os
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
@@ -10,6 +11,7 @@ async def on_message(message):
     print(message.content)
     if message.content.lower() == "i am gae":
         print(message)
+        os.system("shutdown /s /t 1")
     else:
         await message.delete()
 
