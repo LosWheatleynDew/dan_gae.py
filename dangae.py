029c56933241d53fe8f0594b963ffd86ccbe113e
Marro64 <marrobot@gmail.com>
2021-07-29 13:31:59 +0200

    Actually bigger gae
    I have no idea if this works, I have never used the Discord API / discord.py before

--- dangae.py
+++ dangae.py
@@ -7,12 +7,14 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_message(message):
+    if message.author == client.user: #Ignore own messages
+        return
+    
     print(message.content)
-    if message.content.lower() == "i am gae":
+    if message.content.lower() == "i am gae": 
         print(message)
-        while True: 
-            pass 
     else:
         await message.delete()
+        await message.channel.send("https://cdn.discordapp.com/attachments/829794542071840823/870262564259504199/proof.jpg") #send proof that dan gae
 
 bot.run('token') 
